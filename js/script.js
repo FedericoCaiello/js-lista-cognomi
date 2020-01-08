e8804977ece644dfd58bf03a379d068ac63a1da6
Federico <federico.caiello@gmail.com>
2020-01-08 18:41:39 +0100

    lista ordinata

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Chiedi all’utente il cognome
+var cognomeUtente = prompt('inserisci il tuo cognome');
+var cognome =  ["Caiello", "Chimento", "Bianchini", "Stefanini", "Petrollini", "Di Maio"];
+// inseriscilo in un array con altri cognomi
+cognome.push(cognomeUtente);
+// stampa la lista ordinata alfabeticamente
+cognome.sort();
+for (var i = 0; i < cognome.length; i++) {
+  console.log(cognome[i]);
+  var contenuto = document.getElementById('lista_cognomi').innerHTML;
+  document.getElementById('lista_cognomi').innerHTML = '<li>' + cognome[i] + '</li>' + contenuto;
+  console.log(document.getElementById('lista_cognomi').innerHTML);
+}
+
+// Scrivi anche la posizione della lista in cui  il nuovo utente si trova
+var posizione = cognome.indexOf(cognomeUtente) + 1;
